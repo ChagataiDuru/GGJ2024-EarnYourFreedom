d45f2e5d04d823f19bda44e12c136fb48cc91c40
WornBear <mehmetfatihkaraosman67@hotmail.com>
2024-01-28 01:29:28 +0300

    son hal

---
+++ Objects/Character/Scripts/States/State(1).gd
@@ -0,0 +1,19 @@
+extends Node
+
+class_name State
+
+@export var  can_move : bool = true
+
+var playback  :  AnimationNodeStateMachinePlayback
+var character : CharacterBody2D
+
+func handle_input(event : InputEvent):
+	pass
+	
+func on_enter():
+	pass
+func on_exit():
+	pass
+func state_process(delta):
+	pass
+signal on_transition(new_state: State)
---
+++ Objects/Character/Scripts/States/StateMachine(1).gd
@@ -0,0 +1,42 @@
+extends Node
+
+class_name StateMachine
+
+var states : Array[State]
+
+
+@export var current_state : State
+@export var character : CharacterBody2D
+@export var animation_tree : AnimationTree
+
+
+func _ready():
+	for child in get_children():
+		if(child is State):
+			# TODO set up states
+			child.character = character
+			child.playback= animation_tree["parameters/playback"]
+			states.append(child)
+			
+		else:
+			push_warning("Child " + child.name  + " is not a State.")
+			
+
+func _physics_process(delta):
+	current_state.state_process(delta)
+	
+func check_can_move():
+	return current_state.can_move
+	
+func _input(event:InputEvent):
+	current_state.handle_input(event)
+	
+func _on_transition(new_state):
+	if(current_state):
+		current_state.on_exit()
+		
+	current_state= new_state
+	current_state.on_enter()
+
+func get_current_state_name():
+	return current_state.name
